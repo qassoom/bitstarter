9221d2732992a1ea7c3578d6f424ba43c58cbc36
Qasim Al Khuzaie <qrasool@gmail.com>
2013-08-04 09:09:09 +0000

    add a small comment there

--- web.js
+++ web.js
@@ -3,15 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var filename = "./index.html";
-var file = fs.readFileSync(filename,'utf8');
+var htmlfile = "index.html";
+
+//var file = fs.readFileSync(filename,'utf8');
 
 
 app.get('/', function(request, response) {
-  response.send(file);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
